9758b1e5f6c92757fc88042e43c5fba6a9a0d5b6
Jon_shen <jerosshen@gmail.com>
2017-02-06 15:11:45 +0800

    slide change finish

--- dist/jslide/jslide.js
+++ dist/jslide/jslide.js
@@ -9,12 +9,13 @@
         showImgcout: 1,  // 已经显示的幻灯片数，默认是一个
         imgStart: 0,
         direction: 'horizontal',  // or 'vertical'
-        autoplay: true  // or 'false'
+        autoplay: true,  // or 'false'
+        transition: 500
       }
       option = option || {};
       for (var prop in defaultOption) {
-        if ('undefined' === option.prop) {
-            option.prop = defaultOption.prop;
+        if (!option[prop]) {
+            option[prop] = defaultOption[prop];  // 不能用点
         }
       }
 
@@ -25,29 +26,33 @@
       t.btn = t.slideRoot.find('.btn');
       t.imgCount = t.wrap.find('li').length;  // 总数
       t.count = t.imgCount - option.showImgcout;  // 切换次数
-      t.distance = t.wrap.find('li').width;  // 切换的距离
-      t.i = option.imgStart;  // 初始化后的第一张幻灯片， 默认第一张
+      t.distance = t.wrap.width();  // 切换的距离
+      this.i = option.imgStart;  // 初始化后的第一张幻灯片， 默认第一张
       t.styleToDeal = defaultOption.direction === 'horizontal' ? 'margin-left' : 'margin-top';
+      t.wrap.find('li').width(t.distance)
       this.init();
     }
 
     Jonslide.prototype = {
       constructor: Jonslide,
       init: function() {
+        this.slide.css('transition', 'all 0.5s');
         this.clickBtn();
         if (this.params.autoplay) {
           this.autoplay();
         }
       },
       next: function (){
+        // return false;
         var t = this;
+        var styleToDeal = t.styleToDeal;
         if (t.i < t.count) {
-          var styleToDeal = t.styleToDeal;
+
           var maginLeft = parseInt(t.slide.css(t.styleToDeal), 10);
-          t.slide.animate({styleToDeal: maginLeft - t.distance}, 500);
+          t.slide.css(styleToDeal, maginLeft-t.distance);
           t.i++;
         } else {
-          t.box.animate({styleToDeal: 0}, 500);
+          t.slide.css(styleToDeal, 0);
           t.i = 0;
         }
       },
@@ -55,20 +60,22 @@
         var t = this;
         if (t.i>0) {
           var styleToDeal = t.styleToDeal;
-          var maginLeft = parseInt(t.box.css(styleToDeal), 10);
-          t.box.animate({styleToDeal: maginLeft + t.distance}, 500);
+          var maginLeft = parseInt(t.slide.css(styleToDeal), 10);
+          t.slide.animate({styleToDeal: maginLeft + t.distance}, 500);
           t.i--;
         } else {
-          t.box.animate({styleToDeal: -t.distance * t.count}, 500);
+          t.slide.animate({styleToDeal: -t.distance * t.count}, 500);
           t.i = t.count;
         }
       },
       autoplay: function () {
-        this.timer = setInterval(this.next, 2000);
-        this.slide.hover(function() { // 鼠标悬停事件
-          clearInterval(this.timer);
+        var t = this;
+        t.timer = setInterval(t.next.bind(t), 2000);
+        t.slide.hover(function() { // 鼠标悬停事件
+          console.log('该停止了');
+          clearInterval(t.timer);
         }, function() {
-          this.timer = setInterval(this.next, 2000);
+          t.timer = setInterval(t.next.bind(t), 2000);
         });
       },
       clickBtn: function (){
@@ -80,10 +87,10 @@
           } else if ($(this).hasClass('prev')) {
             t.prev();
           }
-        })
+        });
       }
     }
 
-
+  window.Jonslide = Jonslide;
   })
 })(jQuery)
