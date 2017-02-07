a32729bf1ce90cc0e142df442aa8508db1239832
Jon_shen <jerosshen@gmail.com>
2017-02-07 11:31:08 +0800

    finish it

--- dist/jslide/jslide.js
+++ dist/jslide/jslide.js
@@ -8,12 +8,12 @@
       var defaultOption = {
         showImgcout: 1,           // 已经显示的幻灯片数，默认是一个
         imgStart: 0,
-        direction: 'vertical',  // or 'vertical'
+        direction: 'horizontal',    // or 'vertical'
+        // direction: 'vertical',    // or 'vertical'
         autoplay: true,           // or 'false'
-        circle: true,            // 按钮
+        circle: true,             // 按钮
         transition: 500,          // 动画时长
-        timer: 3500,              // 事件间隔
-
+        timer: 3500,              // 切换间隔
       }
       option = option || {};
       for (var prop in defaultOption) {
@@ -26,24 +26,17 @@
       t.slideRoot = $(ele);  // 获取根元素，用户定义
       t.wrap = t.slideRoot.find('.slideWrap');  // 整个幻灯片的包裹层
       t.slide = t.wrap.find('.slide');  // 幻灯片图片的包裹层
-      t.btn = t.slideRoot.find('.btn');
       t.li = t.wrap.find('li');
+      t.btn = t.slideRoot.find('.btn');
+
       t.imgCount = t.li.length;  // 总数
       t.count = t.imgCount - option.showImgcout;  // 切换次数
       t.i = option.imgStart;  // 初始化后的第一张幻灯片， 默认第一张
 
-      var wrapWidth = t.wrap.width();
-      t.li.width(wrapWidth);  // 设置幻灯片大小与最外层相同
-
-
-      if (option.direction === 'horizontal') {
-        t.slide.width(wrapWidth*t.imgCount)
-        t.distance = wrapWidth;
+      if (this.params.direction === 'horizontal') {
         t.styleToDeal = 'margin-left';
         t.li.css('display', 'inline-block');
       } else {
-        t.distance = t.li.height();
-        t.wrap.height(t.distance);
         t.styleToDeal = 'margin-top';
       }
 
@@ -54,7 +47,9 @@
       constructor: Jonslide,
       init: function() {
         this.slide.css('transition', 'all 0.5s');
+        this.direction();
         this.clickBtn();
+        this.resize();
         if (this.params.circle) {
           this.circleAdd();
           this.circleClick();
@@ -63,11 +58,28 @@
           this.autoplay();
         }
       },
+      direction: function(val){
+        var t = this;
+        if (this.params.direction === 'horizontal') {
+          var wrapWidth = t.slideRoot.width();  // 直接获取跟元素有bug， 小于原值15像素
+          if (val) {
+            wrapWidth = val;
+          }
+          t.distance = wrapWidth;
+          t.li.width(wrapWidth);  // 设置幻灯片大小与最外层相同
+          t.distance = wrapWidth;
+
+        } else {
+          t.distance = t.li.height();
+          t.wrap.height(t.distance);
+        }
+        t.slide.css(t.styleToDeal, -t.distance * t.i);
+      },
       next: function (){
         var t = this;
         var styleToDeal = t.styleToDeal;
         if (t.i < t.count) {
-          console.log('下一页');
+          // console.log('下一页');
           var maginLeft = parseInt(t.slide.css(t.styleToDeal), 10);
           t.slide.css(styleToDeal, maginLeft-t.distance);
           t.i++;
@@ -97,7 +109,9 @@
       },
       autoplay: function () {
         var t = this;
-        t.goInterval();
+        setTimeout(function() {
+          t.goInterval()
+        }, t.params.timer)
 
         t.wrap.hover(function() { // 鼠标悬停事件
           t.btn.fadeIn();
@@ -120,9 +134,7 @@
       },
       circleAdd: function(){
         var t = this;
-        console.log(1);
         for (var i = 0; i < t.imgCount; i++) {
-          console.log(2);
           $('.circle-wrap').append('<div class="circle" data-slide=' + i + '>.</div>')
         }
         t.circle = $('.circle');
@@ -145,7 +157,24 @@
           t.slide.css(t.styleToDeal, -t.distance * t.i);
           t.circleChange();
         })
+      },
+      resize: function() {
+        var t = this;
+        var testw1 = this.slideRoot.width();
+        var testw2 = this.slideRoot.parent().width();
+        this.direction(testw1);
+        $(window).resize(function(){
+          clearTimeout(t.timer);
+          throtle(t.direction, t);
+        })
       }
+    }
+
+    var throtle = function (method, context) {  // 函数节流
+      if (method.tid) clearTimeout(method.tid);
+      method.tid = setTimeout(function(){
+        method.call(context);
+      }, 200);
     }
 
   window.Jonslide = Jonslide;
--- dist/util.tool.js
+++ dist/util.tool.js
@@ -1,20 +1,21 @@
-// 判定是否是移动版
-var isMobile = function () {
-   return navigator.userAgent.indexOf('Mobile') !== -1 || navigator.userAgent.indexOf('Android') !== -1;
+
+var util = {
+  isMobile: function () {  // 判定是否是移动版
+     return navigator.userAgent.indexOf('Mobile') !== -1 || navigator.userAgent.indexOf('Android') !== -1;
+  },
+  getLocationOrigin: function () {  // 获取带协议的host地址，有不支持 window.location.origin
+      var origin = window.location.origin;
+      if (!window.location.origin) {
+          origin = '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+      }
+      return origin;
+  },
+  throtle: function (method, context) {  // 函数节流
+    clearTimeout(method.tid);
+    method.tid = setTimeout(function(){
+      method.call(context);
+    }, 200);
+  }
 }
 
-// 获取带协议的host地址，有不支持 window.location.origin
-var getLocationOrigin =function () {
-    var origin = window.location.origin;
-    if (!window.location.origin) {
-        origin = "//" + window.location.hostname + (window.location.port ? ":" + window.location.port : "");
-    }
-    return origin;
-}
-
-// setTimeout 来模拟 setInterval
-var i = 0
-var setTimeInterval = function time(){ //每隔1秒让++i
- console.log(++i);
- setTimeout(time, 1000);
-}
+window.util = util;
